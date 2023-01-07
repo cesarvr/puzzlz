ac32f9413efa70b3c83fbc063694bf9824b4e168
Cesar <cesar@gosub.biz>
2023-01-07 16:43:49 +0100

    done

--- cpp/day-19/main.cpp
+++ cpp/day-19/main.cpp
@@ -90,7 +90,7 @@ int get_max_len(Molecules& molecules){
 }
 
 int get_progress(std::string& candidate, std::string& target){
-    int limit = std::min(candidate.size(), target.size());
+    int limit = std::min((int) candidate.size(), (int) target.size());
     int progress_counter = 0;
     for(progress_counter=0; progress_counter<limit; progress_counter++){
         if(candidate[progress_counter] != target[progress_counter]){
@@ -100,41 +100,98 @@ int get_progress(std::string& candidate, std::string& target){
     return progress_counter;
 }
 
+std::vector<std::string> remove_repeated(std::vector<std::string> &samples){
+    std::vector<std::string> ret;
+    Mutation muts;
+    for(auto &sample: samples){
+        if(muts.find(sample) == muts.end()){
+            ret.push_back(sample);
+            muts[sample] = true;
+        }
+    }
+    return ret;
+}
+
+std::vector<std::string> remove_low_performing(std::vector<std::string> &samples, std::string target, int max_progress, double delta){
+    std::vector<std::string> ret;
+
+    for(auto &sample: samples){
+        if(get_progress(sample, target) >= (max_progress*delta)){
+            ret.push_back(sample);
+        }
+    }
+    return ret;
+}
+
+
 int solve_problem_2(Puzzle19& p){
     auto seeds = p.molecules["e"];
     p.molecules.erase("e");
     int cycles = 2;
     Mutation molecules_states;
     int padding = get_max_len(p.molecules);
+    int max_progress = -1;
     std::cout << "padding -> " << padding << std::endl;
+    std::vector<std::string> samples;
 
     while(true){
         for(auto seed: seeds){
-            molecules_states = solution1(p.molecules, seed);
+            int progress = get_progress(seed, p.target);
+            int from = progress - padding;
+            max_progress = std::max(max_progress, progress);
+
+            from = (from > 0)? from : 0;
+
+            //ORnPBPMgAr
+            //if(progress >= 4){
+            if(seed == "Ca"){
+                std::cout << "dbg";
+            }
+
+            std::string in = seed.substr(from, 6);
+            molecules_states = solution1(p.molecules, in);
+
+            for(auto &mol: molecules_states){
+                std::string w = mol.first;
+                std::string base = seed;
+                std::string _smpl = base.replace(from, 6, w);
+
+                samples.push_back(_smpl);
+            }
         }
 
-        if(molecules_states.find(p.target) != molecules_states.end()){
-            std::cout << "target -> " << p.target << std::endl;
-            std::cout << "steps -> " << cycles << std::endl;
-            return cycles;
+        samples = remove_repeated(samples);
+        if(max_progress > 6 && max_progress < 80) {
+            samples = remove_low_performing(samples, p.target, max_progress, 0.9);
         }
+
+        if(max_progress >= 80) {
+            samples = remove_low_performing(samples, p.target, max_progress, 0.98);
+        }
+
 
         seeds.clear();
-        for(auto& kv: molecules_states){
-            std::string key = kv.first;
-            int progress = get_progress(key, p.target);
-            seeds.push_back(key);
+        for(auto &sample: samples){
+            //if(get_progress(sample, p.target) == p.target.size()){
+            if(sample == p.target){
+                std::cout << "target -> " << sample << std::endl;
+                std::cout << "steps -> " << cycles << std::endl;
+                std::cout << "size: " << samples.size() << std::endl;
+                return cycles;
+            }
+
+            seeds.push_back(sample);
         }
 
-        molecules_states.clear();
+        samples.clear();
         cycles++;
     }
 }
 
 int main() {
     auto puzzle_input = load_file("prod.dat");
-    solve_problem_1(puzzle_input);
-    //solve_problem_2(puzzle_input);
+    //solve_problem_1(puzzle_input);
+    solve_problem_2(puzzle_input);
 
     return 0;
 }
