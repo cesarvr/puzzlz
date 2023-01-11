550062ab5d61b02fc8d911f84da3cfcb559758c3
Cesar <cesar@gosub.biz>
2023-01-11 10:50:08 +0100

    adding timing function

--- aoc_2015/day-4/main.py
+++ aoc_2015/day-4/main.py
@@ -1,7 +1,17 @@
 import hashlib
+import time
+
 input = "ckczppom"
 f1 = b"abcdef"
 f2 = b"609043"
+
+
+def time_this_func(fn):
+    start = time.time()
+    fn()
+    end = time.time()
+    print("time it took: ", end - start)
+
 
 def check_zeroes(intake, zeroes_length=5):
     intake = str(intake)
@@ -15,7 +25,7 @@ def check_zeroes(intake, zeroes_length=5):
 
 def solving_puzzle(len=5):
     for i in range(1000000000):
-        input_in_bytes = str.encode(input+str(i))
+        input_in_bytes = str.encode(input + str(i))
         result = hashlib.md5(input_in_bytes).hexdigest()
         r = check_zeroes(result, len)
 
@@ -24,6 +34,6 @@ def solving_puzzle(len=5):
             break
 
 
+time_this_func(lambda: solving_puzzle(5))
+time_this_func(lambda: solving_puzzle(6))
 
-solving_puzzle(5)
-solving_puzzle(6)
